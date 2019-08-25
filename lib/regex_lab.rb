3d883e6a63891f3e6c55afe0fb52b4f17c99e60a
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-25 01:09:36 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -17,6 +17,9 @@ end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
   if text.match(/^[A-Z]/) && text.match(/$[.]/)
+    true
+  else
+    false
 end
 
 def valid_phone_number?(phone)
