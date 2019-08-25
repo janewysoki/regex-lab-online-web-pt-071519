8f753c8a1a647cebd37cff36f5ee98b2b1eb3668
Jane Wysoki <jane.wysoki@gmail.com>
2019-08-25 00:54:33 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/^[aeiou]|^[AEIOU]/)
+  if word.match(/^[aeiou]|^[AEIOU]/)
     true
   else
     false
@@ -7,7 +7,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.grep(/^/)
 end
 
 def words_five_letters_long(text)
